935df73df172cdec7171616f300f0653ea9fae0e
nappaillav <valliappan0808@gmail.com>
2026-09-11 21:51:44 -0400

    launch_scripts

--- mag_profile/env_launcher.sh
+++ mag_profile/env_launcher.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --account=def-dpmeger
-#SBATCH --job-name=mag_exp
+#SBATCH --job-name=27_mag_exp
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus=a100_3g.20gb:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=24G
 #SBATCH --time=11:59:00
-#SBATCH --array=0-7
+#SBATCH --array=0-1
 #SBATCH --output="/home/chidamv/scratch/logs/log4/out/%x-%A_%a.out"
 #SBATCH --error="/home/chidamv/scratch/logs/log4/log/%x-%A_%a.err"
 
@@ -22,14 +22,28 @@ module load StdEnv/2023 python/3.10 cuda/12.2 rust/1.85.0
 ### -------------------------
 ### Activate virtual environment
 ### -------------------------
-# VENV=${VENV:-/home/chidamv/env/mbmarlEnv}
 source /home/chidamv/env/mbmarlEnv/bin/activate
 
 ### -------------------------
-### Repository
+### Repository / Algorithm
 ### -------------------------
-REPO=${1:-/home/chidamv/projects/def-dpmeger/chidamv/three-baselines/MAG}
-cd $REPO
+
+REPOS=(
+    # "/home/chidamv/projects/def-dpmeger/chidamv/three-baselines/MABL/mabl"
+    "/home/chidamv/projects/def-dpmeger/chidamv/three-baselines/MAG"
+    "/home/chidamv/projects/def-dpmeger/chidamv/three-baselines/MAG_2"
+)
+
+ALGORITHMS=(
+    # "MABL"
+    "MAG"
+    "MAG_2"
+)
+
+REPO=${REPOS[$SLURM_ARRAY_TASK_ID]}
+ALGO=${ALGORITHMS[$SLURM_ARRAY_TASK_ID]}
+
+cd "$REPO" || exit 1
 
 ### -------------------------
 ### StarCraft II
@@ -45,26 +59,15 @@ export WANDB_MODE=offline
 wandb offline
 
 ### -------------------------
-### Experiment configurations
+### Experiment configuration
 ### -------------------------
-MAPS=(
-    # "27m_vs_30m"
-    "3s5z_vs_3s6z"
-    "MMM2"
-    "3s_vs_4z"
-    "3s_vs_5z"
-    "6h_vs_8z"
-    "corridor"
-    "so_many_baneling"
-    "2c_vs_64zg"
-)
 
-MAP=${MAPS[$SLURM_ARRAY_TASK_ID]}
+MAP=${1:-"3s5z_vs_3s6z"}
 
 ### -------------------------
 ### Training configuration
 ### -------------------------
-STEPS=${STEPS:-2000000}
+STEPS=${2:-3000000}
 N_WORKERS=4
 
 CMD="python train.py \
@@ -80,6 +83,7 @@ echo "================================================"
 echo "JOB ID:           $SLURM_JOB_ID"
 echo "ARRAY JOB ID:     $SLURM_ARRAY_JOB_ID"
 echo "TASK ID:          $SLURM_ARRAY_TASK_ID"
+echo "ALGORITHM:        $ALGO"
 echo "ENV:              $MAP"
 echo "STEPS:            $STEPS"
 echo "WORKERS:          $N_WORKERS"
@@ -97,12 +101,12 @@ nvidia-smi --query-gpu=name,memory.total --format=csv
 ### -------------------------
 ### Run experiment
 ### -------------------------
-eval $CMD
+eval "$CMD"
 
 ### -------------------------
 ### Runtime
 ### -------------------------
-echo "Experiment task $SLURM_ARRAY_TASK_ID ($MAP) finished."
+echo "Experiment task $SLURM_ARRAY_TASK_ID ($ALGO, $MAP) finished."
 
 end=$(date +%s)
 runtime=$((end - start))
